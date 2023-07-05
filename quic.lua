96cd3d13965a949fc4eea97a40b93a272d86ff37
morjun <anbndn5@gmail.com>
2023-07-05 23:42:38 +0900

    Add dissector example from wireshark doc and revise quic.lua

--- quic.lua
+++ quic.lua
@@ -1,56 +1,53 @@
 do
-    local p_quic = Proto ("quic_http3", "Quic proto for http3")
+    local p_quic = Proto ("quic_d9c", "Quic proto version draft 9 custom")
 
     local f = p_quic.fields
 
+    f.f_quic_flags = ProtoField.uint8 ("quic.flags","QUIC Flags")
+    f.f_quic_short = ProtoField.uint8 ("quic.short","QUIC Short Header")
+    f.f_quic_long = ProtoField.uint8 ("quic.long","QUIC Long Header")
+
+    f.f_quic_DCI = ProtoField.uint64("quic.DCI","Dst Connection ID")
+
     f.f_quic_version = ProtoField.uint32 ("quic.version","QUIC Version")
-    f.f_quic_long_package_flag = ProtoField.uint8 ("quic.long_package_flag","long package Flag", base.DEC, nil, 0x80)
-    f.f_quic_DCIL = ProtoField.uint8 ("quic.DCIL","Dst Connection ID Len", base.DEC, nil, 0xf0)
-    f.f_quic_real_DCIL = ProtoField.uint8 ("quic.DCIL","Dst Connection ID Len after cal")
-    f.f_quic_real_SCIL = ProtoField.uint8 ("quic.SCIL","Src Connection ID Len after cal")
-    f.f_quic_SCIL = ProtoField.uint8 ("quic.SCIL","Src Connection ID Len", base.DEC, nil, 0x0f)
-    f.f_quic_SCI = ProtoField.bytes("quic.SCI","Src Connection ID")
-    f.f_quic_DCI = ProtoField.bytes("quic.DCI","Dst Connection ID")
+
+    f.f_quic_packetNumber = ProtoField.uint32 ("quic.packetNumber","QUIC Packet Number")
+    -- f.f_quic_DCIL = ProtoField.uint8 ("quic.DCIL","Dst Connection ID Len", base.DEC, nil, 0xf0)
+    -- f.f_quic_real_DCIL = ProtoField.uint8 ("quic.DCIL","Dst Connection ID Len after cal")
+    -- f.f_quic_SCIL = ProtoField.uint8 ("quic.SCIL","Src Connection ID Len", base.DEC, nil, 0x0f)
+    -- f.f_quic_real_SCIL = ProtoField.uint8 ("quic.SCIL","Src Connection ID Len after cal")
+    -- f.f_quic_SCI = ProtoField.bytes("quic.SCI","Src Connection ID")
 
     function p_quic.dissector(tvb, pinfo, tree)
-        pinfo.cols.protocol = "QUIC/HTTP3.0"
-        local subtree = tree:add (p_quic, tvb())
+        pinfo.cols.protocol = "QUIC/DRAFT-9-CUSTOM"
+        local subtree = tree:add (p_quic, tvb()) -- tvb: packet's buffer
         local offset = 0
 
-        local first_oct = tvb(offset, 1)
+        local first_oct = tvb(offset, 1) -- 1 byte field
         offset = offset + 1
-        subtree:add (f.f_quic_long_package_flag, 0xf0)
+        subtree:add (f.f_quic_flags, first_oct)
+
+        -- TODO: flag 파싱 -> tvb.bitfiled 활용할 것
+
+        local dci = tvb(offset, 8)
+        offset = offset + 8
+        subtree:add (f.f_quic_DCI, dci)
+
+        -- TODO: Long Header일 때만 Version 필드 추가
+
         local version = tvb(offset, 4)
         offset = offset + 4
         subtree:add (f.f_quic_version, version)
 
-        local CI_len = tvb(offset, 1)
-        offset = offset + 1
-        subtree:add (f.f_quic_DCIL, CI_len)
-        subtree:add (f.f_quic_SCIL, CI_len)
+        -- TODO: Short Header인지 검사하고, Short일 경우 가변 길이 패킷 번호 구현
 
-        local DCIL = bit32.rshift(bit32.band(CI_len:uint(), 0xf0), 4)
-        local SCIL = bit32.band(CI_len:uint(), 0x0f)
-        if(DCIL > 0) then
-            subtree:add (f.f_quic_real_DCIL, DCIL + 3)
-            local dst_connection_id = tvb(offset, DCIL + 3)
-            offset = offset + 3 + DCIL
-            subtree:add (f.f_quic_DCI, dst_connection_id)
-        else
-            subtree:add (f.f_quic_real_DCIL, DCIL)
-        end
-        if(SCIL > 0) then
-            subtree:add (f.f_quic_real_SCIL, SCIL + 3)
-            local src_connection_id = tvb(offset, SCIL + 3)
-            offset = offset + 3 + SCIL
-            subtree:add (f.f_quic_SCI, src_connection_id)
-        else
-            subtree:add (f.f_quic_real_SCIL, SCIL)
-        end
+        local packetNumber = tvb(offset, 4)
+        offset = offset + 4
+        subtree:add (f.f_quic_packetNumber, packetNumber)
 
     end
 
     -- Register the dissector
     local udp_encap_table = DissectorTable.get("udp.port")
-    udp_encap_table:add(6121,p_quic)
+    udp_encap_table:add(49153,p_quic)
 end
